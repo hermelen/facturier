10867824131821c5a7e5bb4cd5a52d3e781215b8
hermelen <hermelen.peris@gmail.com>
2018-10-02 11:00:54 +0200

    class customer et product ok

---
+++ app/models.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+# from django.db import models
+from django.contrib.gis.db import models
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name="Prénom", null=True, blank=True)
+    last_name  = models.CharField(max_length=100, verbose_name="Nom", null=True, blank=True)
+    company    = models.CharField(max_length=100, verbose_name="Entreprise", null=True, blank=True)
+    email      = models.EmailField(max_length=200, verbose_name="E-mail", null=True, blank=True)
+    address    = models.CharField(max_length=150, verbose_name="Adresse", null=True, blank=True)
+    zipcode    = models.CharField(max_length=10, verbose_name="Code Postal", null=True, blank=True)
+    city       = models.CharField(max_length=50, verbose_name="Ville", null=True, blank=True)
+
+    def __unicode__(self):
+        return self.last_name
+
+
+class Product(models.Model):
+    name        = models.CharField(max_length=100, verbose_name="Nom", null=True, blank=True)
+    short_desc  = models.TextField(verbose_name="En bref", null=True, blank=True)
+    decription  = models.TextField(verbose_name="Description", null=True, blank=True)
+    price       = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Price", null=True, blank=True)
+    ref         = models.CharField(max_length=150, verbose_name="Référence", null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+# class ProductList(models.Model):
+
+
+
+
--- facturier/settings.py
+++ facturier/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_URL = '/media/'
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "node_modules"),
+# ]
+
+LOGIN_URL = "/login"
+LOGOUT_URL ="/logout"
+
+LOGIN_REDIRECT_URL = '/'
