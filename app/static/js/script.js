3734e223ace5e6d8aac6c28cf4fbf864bb01a0f4
hermelen <hermelen.peris@gmail.com>
2018-10-11 16:43:57 +0200

    x-editable debut

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
     $(function() {
         $('#quotation-form div').formset();
     })
+    $.fn.editable.defaults.mode = 'inline';
+    $('.x-editable').editable();
+
+    $('.x-editable').click(function(){
+      $('div.editable-input input' ).keypress(function() {
+        // $(this).closest('.single-line').css('background-color', 'blue');
+        console.log($(this).closest('.single-line:nth-child(3)'));
+      });
+    });
 })
