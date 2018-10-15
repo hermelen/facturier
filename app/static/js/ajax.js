27e931aacc54ef9d4d227f2ed9e18257f0d5c9b8
hermelen <hermelen.peris@gmail.com>
2018-10-15 13:33:27 +0200

    add product en cours suite

--- app/static/js/ajax.js
+++ app/static/js/ajax.js
@@ -19,10 +19,29 @@ function deleteCommand(idButton) {
 }
 
 $('#line-to-add').hide();
+$('.create-btn').hide();
+
 
 $('.add-btn').click(function(){
+  $('.create-btn').show();
   $('#line-to-add').show();
   $('.create-btn').click(function(){
-    var url = $(this).attr('action');
+
   });
 });
+
+function createCommand() {
+  var url = $('.add-btn').attr('action');
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: idButton,
+    success: function() {
+      console.log('success');
+        $('#tr-'+idButton).remove();
+    },
+    error: function () {
+      console.log('error');
+    }
+  });
+}
