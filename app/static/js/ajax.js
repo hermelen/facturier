f22e59ff559b6378368c354d8d5204f1b0da0ec2
hermelen <hermelen.peris@gmail.com>
2018-10-15 17:00:37 +0200

    lundi soir

--- app/static/js/ajax.js
+++ app/static/js/ajax.js
@@ -1,47 +1,43 @@
 $('.delete-btn').click(function(){
-    var idButton = $(this).attr('id')
-    deleteCommand(idButton);
+    var url = $(this).data('url');
+    $.ajax({
+      url: url,
+      type: "POST",
+      success: function() {
+        console.log('success');
+        $(this).remove();
+      },
+      error: function () {
+        console.log('error');
+      }
+    });
 })
-function deleteCommand(idButton) {
-   var url = $('.delete-btn').attr('action');
-   $.ajax({
-     url: url,
-     type: "POST",
-     data: idButton,
-     success: function() {
-       console.log('success');
-         $('#tr-'+idButton).remove();
-     },
-     error: function () {
-       console.log('error');
-     }
-   });
-}
+
 
 $('#line-to-add').hide();
 // $('.create-btn').hide();
 
 
-$('.add-btn').click(function(){
-  // $('.create-btn').show();
-  $('#line-to-add').show();
-  $('.create-btn').click(function(){
-      // createCommand();
-  });
-});
 
-// function createCommand() {
-//   var url = $('.add-btn').attr('action');
-//   $.ajax({
-//     url: url,
-//     type: "POST",
-//     data: idButton,
-//     success: function() {
-//       console.log('success');
-//         $('#tr-'+idButton).remove();
-//     },
-//     error: function () {
-//       console.log('error');
-//     }
-//   });
-// }
+$('#line-to-add').show();
+
+
+$(document).ready(function(){
+  $('#product_list_form').submit(function(event){
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: $(this).serializeArray(),
+      success: function(response) {
+        console.log(response);
+        //         $('#tr-'+idButton).remove();
+      },
+      error: function () {
+        console.log('error');
+      }
+    });
+    event.preventDefault();
+  });
+
+})
